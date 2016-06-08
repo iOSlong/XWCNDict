79addc447c615accc411555afa1121ae6e40dc6b
xuewu <xuewu1011@163.com>
2016-06-08 23:17:10 +0800

    创建工程

---
+++ XWCNDict/XWCNDict/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XWCNDict
+//
+//  Created by xw.long on 16/6/8.
+//  Copyright © 2016年 linxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
