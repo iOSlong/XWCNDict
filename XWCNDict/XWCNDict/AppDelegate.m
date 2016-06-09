4294d18f36bda927f25b8e2650bdbbdc951ea1b3
xuewu <xuewu1011@163.com>
2016-06-09 15:02:15 +0800

    调整pch文件结构

--- XWCNDict/XWCNDict/AppDelegate.m
+++ XWCNDict/XWCNDict/AppDelegate.m
@@ -29,6 +29,7 @@
 @implementation AppDelegate
 
 
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
 
