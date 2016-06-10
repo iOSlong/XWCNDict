8e3fb2a67dfae247ed5b2952ac0242f6578d0924
xuewu <xuewu1011@163.com>
2016-06-10 23:19:55 +0800

    添加图片资源，setXCCharacterViewController

--- XWCNDict/XWCNDict/Controller/XWCharactersViewController.m
+++ XWCNDict/XWCNDict/Controller/XWCharactersViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "XWCharactersViewController.h"
+#import "XWCharacterPlat.h"
 
-@interface XWCharactersViewController ()
+@interface XWCharactersViewController ()<UIScrollViewDelegate>
+
+@property (nonatomic) UIScrollView *scrollView;
+@property (nonatomic) XWCharacterPlat *currentCharPlat;
+@property (nonatomic) NSMutableArray *arrCharPlat;
 
 @end
 
@@ -19,22 +24,45 @@
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor yellowColor];
 
+    [self configureScrollView];
+
     
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSMutableArray *)arrCharPlat{
+    if (!_arrCharPlat) {
+        _arrCharPlat = [NSMutableArray array];
+    }
+    return _arrCharPlat;
 }
 
-/*
-#pragma mark - Navigation
+- (void)configureScrollView {
+    self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 470.0/2 * kFixed_rate, 1024 * kFixed_rate, 428 * kFixed_rate)];
+    self.scrollView.contentSize = CGSizeMake(1024*3 * kFixed_rate, 854/2 * kFixed_rate);
+    self.scrollView.showsHorizontalScrollIndicator = NO;
+    self.scrollView.pagingEnabled = YES;
+    self.scrollView.delegate = self;
+    [self.view addSubview:_scrollView];
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    UIImage *backGroundImg = [UIImage imageNamed:@"font_plat.png"];
+    CGFloat width = CGImageGetWidth(backGroundImg.CGImage)/2.0 * kFixed_rate;
+    CGFloat height = CGImageGetHeight(backGroundImg.CGImage)/2.0 * kFixed_rate;
+    CGFloat leftSpan = 158/2 * kFixed_rate;
+    XWCharacterPlat *charPlat1 = [[XWCharacterPlat alloc] initWithFrame:CGRectMake(leftSpan, 0, width, height)];
+    XWCharacterPlat *charPlat2 = [[XWCharacterPlat alloc] initWithFrame:CGRectMake(leftSpan + 1024 * kFixed_rate, 0, width, height)];
+    XWCharacterPlat *charPlat3 = [[XWCharacterPlat alloc] initWithFrame:CGRectMake(leftSpan + 2*(1024 * kFixed_rate), 0, width, height)];
+    charPlat1.backgroundColor = [UIColor whiteColor];
+    charPlat2.backgroundColor = [UIColor whiteColor];
+    charPlat3.backgroundColor = [UIColor whiteColor];
+
+    [self.scrollView addSubview:charPlat1];
+    [self.scrollView addSubview:charPlat2];
+    [self.scrollView addSubview:charPlat3];
+
+
+
 }
-*/
+
+
 
 @end
