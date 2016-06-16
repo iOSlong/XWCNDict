f84f5cb5598988925f7bf32018a4b82b0632f414
xuewu <xuewu1011@163.com>
2016-06-17 01:07:10 +0800

    add source of ReactiveCocoa and rest Podfile with pod init

--- XWCNDict/Pods/Target Support Files/Pods-XWCNDict/Pods-XWCNDict-frameworks.sh
+++ XWCNDict/Pods/Target Support Files/Pods-XWCNDict/Pods-XWCNDict-frameworks.sh
@@ -82,3 +82,14 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/FMDB/FMDB.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/ReactiveCocoa/ReactiveCocoa.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Result/Result.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/FMDB/FMDB.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/ReactiveCocoa/ReactiveCocoa.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Result/Result.framework"
+fi
